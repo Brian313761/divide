27f88dfbd7c1ac3769b2673491a00f3cbb3e248b
Brian313761 <140124219+Brian313761@users.noreply.github.com>
2023-11-03 22:22:20 -0400

    Add files via upload

---
+++ divide/src/divide/numberdivide.java
@@ -0,0 +1,99 @@
+package divide;
+
+import java.util.Scanner;
+
+public class numberdivide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*int number = 7;
+		if (number%2 ==0) {
+			System.out.println("Input number is even");
+	
+		}
+		
+		else {
+			System.out.println("Input number is odd");
+		
+		System.out.println("thr below are even number from 1 to 100:");
+		for(int number=1; number<=100; number++) {
+			if (number%2==0) {
+			System.out.println(number); 	
+	*/
+		
+		
+		/*Scanner invisible = new Scanner (System.in);
+		System.out.println("input a number");
+		int i =invisible.nextInt();
+		if(i%2==0) {
+			System.out.println("Input number is even");
+	
+		}
+	
+		else {
+			System.out.println("Input number is odd"); 
+		}
+			else if (i<0) {
+		System.out.println("so it's positive");
+			}
+		else if (i>0){
+			System.out.println("so it's negative");
+		}
+			else (i=0) {
+				System.out.println("invaided");
+		}*/
+		
+		//int[]score = new int[5];
+		//int [] score= {91,92,93,94,95}; 
+		//System.out.println(score[4]); //	
+		
+		
+		
+		
+		/*String [] student= {"Brian_Le", "Henry_Nguyen", "Brian_Nguyen", "Ken_Nguyen", "Jeana_Doan"};
+		
+		for(int i=0; i<student.length;i++)
+		System.out.println(student[i]); 
+	}*/
+
+	/*
+	System.out.println("   *    ");
+	System.out.println("  * *   ");
+	System.out.println(" *   *   ");
+	System.out.println("*     *  ");
+	System.out.println(" *   *   ");
+	System.out.println("  * *    ");
+	System.out.println("   *    ");
+	*/
+		
+		/*Scanner bruh = new Scanner(System.in);
+		System.out.println("input a first number: ");
+		int firstNumber = bruh.nextInt();
+
+		System.out.println("input a second number");
+		int secondNumber = bruh.nextInt();
+
+		if (firstNumber > secondNumber){
+			
+			System.out.println("number "+ firstNumber +" is greater than number "+ secondNumber);
+			
+		}
+		else if (firstNumber<secondNumber) {
+		System.out.println("number "+ firstNumber +" is less than number "+ secondNumber);
+		}
+		else {
+			System.out.println(firstNumber +" is equal to " +secondNumber );
+	}*/
+		
+		/*int [][] nothing = {{1,2,3},{4,5,6},{7,8,9}};
+		System.out.println(nothing[1][2]);
+		*/
+		
+		
+		}
+}
+
+
+
